bf3e06d5921ccf8109cd64d02516872f51f61ec1
Joshua Buchacher <jbuchacher@me.com>
2016-02-11 13:42:33 -0800

    Add previous/next window switching

--- my/my-keybindings.el
+++ my/my-keybindings.el
@@ -1,13 +1,17 @@
-; GLOBAL
+;; GLOBAL
 (define-key key-translation-map "\C-j" "\C-x")
 (global-set-key (kbd "M-j") nil)
 
-; MAGIT
+;; MAGIT
 (global-set-key (kbd "M-j g") 'magit-status)
 
-; SMEX
+;; SMEX
 (global-set-key (kbd "M-x") 'smex)
 ;; (old meta-x)
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
+;; BUFFER SWITCHING
+(global-set-key (kbd "M-l") 'next-multiframe-window)
+(global-set-key (kbd "M-h") 'previous-multiframe-window)
 
 (provide 'my-keybindings)
