6007e3c801f6125307d6203361e0e9f4abff70fa
Joshua Buchacher <jbuchacher@me.com>
2016-02-12 09:26:30 -0800

    Add default bookmarks folder

--- my/my-package-customizations.el
+++ my/my-package-customizations.el
@@ -1,15 +1,18 @@
 (custom-set-variables
 
- ; IDO
-  '(ido-decorations
+ ;; IDO
+ '(ido-decorations
    (quote
     ("
 >> " "" "
    " "
    ..." "[" "]" " [No match]" " [Matched]" " [Not readable]" " [Too big]" " [Confirm]")))
-   
- ; IDO-UBIQUITOUS
-  '(ido-ubiquitous-mode t)
- )
  
+ ;; IDO-UBIQUITOUS
+ '(ido-ubiquitous-mode t)
+
+ ;; BOOKMARKS
+ '(bookmark-default-file "~/.emacs.d/my/my-bookmarks.el")
+ )
+
 (provide 'my-package-customizations)
