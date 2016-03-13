de6173655f7c58ee754f156419525cd74e68cda6
Joshua Buchacher <jbuchacher@me.com>
2016-03-12 21:24:37 -0800

    Use smex major mode commands

--- my/my-keybindings.el
+++ my/my-keybindings.el
@@ -7,6 +7,8 @@
 
 ;; SMEX
 (global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+
 ;; (old meta-x)
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 
