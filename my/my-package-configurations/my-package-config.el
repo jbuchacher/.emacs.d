d5a57816a63db27bcfcb685703126c0ccdb26492
Joshua Buchacher <jbuchacher@me.com>
2016-09-30 23:18:44 -0700

    Migrate move-dup to use use-package

--- my/my-package-configurations/my-package-config.el
+++ my/my-package-configurations/my-package-config.el
@@ -1,18 +1,6 @@
 ;;; my-packages --- Summary
 ;;; Commentary:
 ;;; Code:
-(require 'package)
-(defvar my-required-packages
-  '(
-    move-dup
-  ) "A list of packages to ensure are installed at launch.")
-
-; method to check if all packages are installed
-(defun packages-installed-p ()
-  "Check if packages are installed."
-  (loop for p in my-required-packages
-        when (not (package-installed-p p)) do (return nil)
-        finally (return t)))
 
 (mapc
  'require
@@ -26,6 +14,7 @@
    company-config
    web-mode-config
    flycheck-config
+   move-dup-config
    ))
 
 (require 'my-package-customizations)
