bcb6f1a6bc61ba5366c94f749661bd6147e30b82
Joshua Buchacher <jbuchacher@me.com>
2016-09-30 23:51:41 -0700

    Migrate web-mode hooks to use-package

--- my/my-hooks.el
+++ my/my-hooks.el
@@ -4,29 +4,4 @@
 ;; Company mode autocompletion
 (add-hook 'after-init-hook 'global-company-mode)
 
-;; WEB MODE
-(add-hook
- 'web-mode-hook
- (lambda ()
-   (linum-mode)
-   (eslint-set-closest-executable)
-   (set-node-modules-path)
-   (flycheck-mode)
-   (setq web-mode-content-types-alist
-         '(("jsx" . "\\.js[x]?\\'")))
-   (setq web-mode-markup-indent-offset 2)
-   (setq web-mode-css-indent-offset 2)
-   (setq web-mode-code-indent-offset 2)
-   (setq web-mode-attr-indent-offset 2)
-   (setq web-mode-attr-value-indent-offset 2)
-   (setq web-mode-indentless-elements 2)
-   (setq web-mode-markup-indent-offset 2)
-   (setq web-mode-sql-indent-offset 2)))
-
-(add-hook
- 'css-mode-hook
- (lambda ()
-   (setq css-indent-offset 2)))
-
-
 (provide 'my-hooks)
