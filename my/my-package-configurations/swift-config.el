f0c5ce3d1de7db771696ffcd948120b8bb39f325
Joshua Buchacher <jbuchacher@me.com>
2017-05-27 07:17:14 -1000

    Remove overriden bindings from swift-mode

--- my/my-package-configurations/swift-config.el
+++ my/my-package-configurations/swift-config.el
@@ -3,7 +3,11 @@
 ;;; Code:
 (use-package swift-mode
   :ensure t
-  )
+  :bind (
+         :map swift-mode-map
+              ("M-j" . nil)
+              ("C-M-j" . nil)
+              ))
 
 (provide 'swift-config)
 ;;; swift-config.el ends here
