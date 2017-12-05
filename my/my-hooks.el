be81e0e38fcabceb50e8725be1d8eac6fdede7bf
Joshua Buchacher <jbuchacher@me.com>
2017-12-05 12:48:26 -0800

    Don't thrash Xcode whitespace handling

--- my/my-hooks.el
+++ my/my-hooks.el
@@ -1,7 +1,12 @@
 ;; Before Save
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
+(add-hook 'before-save-hook (lambda ()
+                              (if (not (eq major-mode 'swift-mode))
+                                (delete-trailing-whitespace)
+                              )))
 
 ;; Company mode autocompletion
 (add-hook 'after-init-hook 'global-company-mode)
+
+
 
 (provide 'my-hooks)
